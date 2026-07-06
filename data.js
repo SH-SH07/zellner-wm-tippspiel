ce610ae2f26657e7bbee27503d06046e26867058
SH-SH07 <s.shamkhani@personal-zellner.at>
2026-07-06 14:07:01 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,103 @@
+const PARTICIPANTS = [
+  {
+    "name": "Lea",
+    "tips": [
+      "Spanien",
+      "Argentinien",
+      "Mexiko",
+      "Marokko"
+    ]
+  },
+  {
+    "name": "Samil",
+    "tips": [
+      "Argentinien",
+      "Frankreich",
+      "Brasilien",
+      "Spanien"
+    ]
+  },
+  {
+    "name": "Nik",
+    "tips": [
+      "Frankreich",
+      "Argentinien",
+      "Spanien",
+      "Brasilien"
+    ]
+  },
+  {
+    "name": "Klaus",
+    "tips": [
+      "Spanien",
+      "Argentinien",
+      "Frankreich",
+      "Brasilien"
+    ]
+  },
+  {
+    "name": "Andreas",
+    "tips": [
+      "Frankreich",
+      "Mexiko",
+      "Spanien",
+      "Schweiz"
+    ]
+  },
+  {
+    "name": "Gela",
+    "tips": [
+      "Portugal",
+      "Frankreich",
+      "Argentinien",
+      "England"
+    ]
+  },
+  {
+    "name": "Andrea",
+    "tips": [
+      "Frankreich",
+      "Brasilien",
+      "Argentinien",
+      "England"
+    ]
+  },
+  {
+    "name": "Shayan",
+    "tips": [
+      "Frankreich",
+      "Argentinien",
+      "Spanien",
+      "England"
+    ]
+  }
+];
+const TEAMS = [
+  "Argentinien",
+  "Belgien",
+  "Brasilien",
+  "England",
+  "Frankreich",
+  "Kolumbien",
+  "Marokko",
+  "Mexiko",
+  "Norwegen",
+  "Portugal",
+  "Schweiz",
+  "Spanien",
+  "USA",
+  "Ägypten",
+  "Österreich"
+];
+const INITIAL_STATE = {
+  "eliminated": [
+    "Brasilien",
+    "Mexiko"
+  ],
+  "finalTop4": [
+    "",
+    "",
+    "",
+    ""
+  ]
+};
